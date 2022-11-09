29e0917172063e72bee74e148d494c704322e989
Jeferson Stiben Pedraza Echeverry <jpedraze@emeal.nttdata.com>
2022-11-09 14:35:20 -0500

    Se crea proceso de reporte

--- src/test/java/com/everis/ct/web/hooks/Hooks.java
+++ src/test/java/com/everis/ct/web/hooks/Hooks.java
@@ -2,8 +2,16 @@ package com.everis.ct.web.hooks;
 
 import com.everis.ct.web.lib.WebDriverManager;
 import com.everis.ct.web.service.stepdefinition.ManageScenario;
+import com.everis.ct.web.util.General;
 import io.cucumber.java.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Hooks {
 
@@ -23,6 +31,12 @@ public class Hooks {
         return Boolean.valueOf(value);
     }
 
+    General general = new General();
+    DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    DateFormat dateFormatCompleto = new SimpleDateFormat("dd:MM:yy HH:mm:ss");
+    Date dateInicial;
+    Date dateFinal;
+
     @Before(order = 0)
     public void setUp() {
         manager.setUpDriver();
@@ -31,6 +45,12 @@ public class Hooks {
     @Before(order = 1)
     public void handleScenario(Scenario scenario) {
         this.scenario.setScenario(scenario);
+    }
+
+    @Before(order = 2)
+    public void timeInitial() {
+        dateInicial = new Date();
+        System.out.println("Hora actual: " + dateFormat.format(dateInicial));
     }
 
     @After(order = 0)
@@ -42,6 +62,39 @@ public class Hooks {
     @After(order = 1)
     public void tearDown() {
         scenario.shotWhenFail();
+    }
+
+    @After(order = 2)
+    public void processReport() {
+      /*
+        //Proceso de hora
+        dateFinal = new Date();
+        System.out.println("Hora actual: " + dateFormat.format(dateFinal));
+        Date difference = general.getDifferenceBetwenDates(dateInicial,dateFinal);
+        System.out.println(dateFormat.format(difference)); //00:02:00
+        try {
+            Thread.sleep(180000);
+        }catch (Exception e){
+            e.getMessage();
+        }
+        //Proceso de creación de carpeta general
+        general.creacionCarpetasReporte(System.getProperty("user.dir") + "//reportes//indicadores");
+        //Proceso de creación de carpeta hora y ejecucion
+        String rutaFechaReporte = System.getProperty("user.dir") + "//reportes//indicadores//"+dateFormatCompleto.format(dateInicial).replaceAll(":",".").replaceAll(" ","_");
+        general.creacionCarpetasReporte(rutaFechaReporte);
+        try {
+            File file =new File(System.getProperty("user.dir")+"\\target\\cucumber\\site\\cucumber-html-reports");
+            if (file.exists()){
+                System.out.println("El archivo cucumber existe");
+            }else{
+                System.out.println("El archivo no cucumber existe");
+            }
+        }catch (Exception e){
+            e.getMessage();
+        }
+        //Proceso de movimiento de reportes
+        general.movimientoDeArchivos(System.getProperty("user.dir")+"\\target\\cucumber\\site\\cucumber-html-reports",rutaFechaReporte);
+*/
     }
 
 }
